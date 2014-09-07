6d93e745660dc37d9b8be9b264d508d085530573
Piotr Szmielew <p.szmielew@ava.waw.pl>
2014-09-07 19:42:49 +0200

    readme + lagrange piecewise-quadratic and piecewise-cubic algorithms

--- README.md
+++ README.md
@@ -5,3 +5,13 @@ Homebrew for Nintendo DS showing interpolation algorithms
 
 Compilation: make
 Requirements: devKitArm with nds support.
+
+Controls:
+
+* touchpad enter points
+* A - draw interpolation
+* X - clean screen and remove points
+* Y - clean screen keeping ponts
+* left/right - change background
+* up/down - change lambda
+* left/right trigger - change interpolation scheme
--- source/main.cpp
+++ source/main.cpp
@@ -23,14 +23,8 @@
 
 using namespace std;
 
-volatile int frame = 0;
 int switch_val = 0;
 int interp_type = 0;
-
-void Vblank() {
-//---------------------------------------------------------------------------------
-	frame++;
-}
 
 void setPixel( int x, int y, int color ) {
     VRAM_A[x + y * 256] = color;
@@ -116,11 +110,69 @@ void drawInterpolationQuadraticLagrange(vector<touchPosition> const &touchPositi
   }
 }
 
+void drawInterpolationCubicLagrange(vector<touchPosition> const &touchPositions, float lambda) {
+  int k = touchPositions.size()-1;
+  if (k % 3 == 1)
+  {
+    k = k-1;
+  }
+  if (k % 3 == 2)
+  {
+    k = k-2;
+  }
+  for(int j=0;j<k;j = j+3) {    
+    int td0 = 0;
+    int n_x = touchPositions[j+1].px - touchPositions[j+0].px;
+    int n_y = touchPositions[j+1].py - touchPositions[j+0].py;
+    float td1 = pow(sqrt(pow(n_x,2) + pow(n_y,2)),lambda);
+    n_x = touchPositions[j+2].px - touchPositions[j+1].px;
+    n_y = touchPositions[j+2].py - touchPositions[j+1].py;
+    float td2 = pow(sqrt(pow(n_x,2) + pow(n_y,2)),lambda) + td1;
+    n_x = touchPositions[j+3].px - touchPositions[j+2].px;
+    n_y = touchPositions[j+3].py - touchPositions[j+2].py;
+    float td3 = pow(sqrt(pow(n_x,2) + pow(n_y,2)),lambda) + td2;
+    int q0_x = touchPositions[j+0].px;
+    int q0_y = touchPositions[j+0].py;
+    int q1_x = touchPositions[j+1].px;
+    int q1_y = touchPositions[j+1].py;
+    int q2_x = touchPositions[j+2].px;
+    int q2_y = touchPositions[j+2].py;
+    int q3_x = touchPositions[j+3].px;
+    int q3_y = touchPositions[j+3].py;
+    float range = td3 - td0;
+    float one_step = range/1000;
+      for(int i=0;i<(td3*(1/one_step));i++) {
+    //    list4points[[1]] + a1 * s + a2 * s^2
+        float t = i*one_step;
+        int x = q0_x*(((t - td1)*(t - td2)*(t - td3))/((td0 - td1)*(td0 - td2)*(td0 - td3))) + 
+          q1_x*(((t - td0)*(t - td2)*(t - td3))/((td1 - td0)*(td1 - td2)*(td1 - td3))) +
+          q2_x*(((t - td0)*(t - td1)*(t - td3))/((td2 - td1)*(td2 - td0)*(td2 - td3))) +
+          q3_x*(((t - td0)*(t - td1)*(t - td2))/((td3 - td1)*(td3 - td0)*(td3 - td2)));
+
+        int y = q0_y*(((t - td1)*(t - td2)*(t - td3))/((td0 - td1)*(td0 - td2)*(td0 - td3))) + 
+          q1_y*(((t - td0)*(t - td2)*(t - td3))/((td1 - td0)*(td1 - td2)*(td1 - td3))) +
+          q2_y*(((t - td0)*(t - td1)*(t - td3))/((td2 - td1)*(td2 - td0)*(td2 - td3))) +
+          q3_y*(((t - td0)*(t - td1)*(t - td2))/((td3 - td1)*(td3 - td0)*(td3 - td2)));
+        // iprintf("\x1b[16;0H x = %d, y = %d\n", x,y);
+        setPixel(x,y,255);
+      }
+
+  }
+}
+
 void drawInterpolation(vector<touchPosition> const &touchPositions, float lambda) {
-  // if (interp_type == 0)
+  if (interp_type == 0)
     drawInterpolationQuadraticLagrange(touchPositions, lambda);
-  // if (inter_type == 1)
-   // drawInterpolationCubicLagrange(touchPositions, lambda);
+  if (interp_type == 1)
+    drawInterpolationCubicLagrange(touchPositions, lambda);
+}
+
+
+void printInterp() {
+  if (interp_type == 0) 
+    iprintf("\x1b[17;0HLagrange Quadratic");
+  if (interp_type == 1) 
+    iprintf("\x1b[17;0HLagrange Cubic       ");
 }
 
 void drawBg() {
@@ -133,6 +185,7 @@ void drawBg() {
   if (switch_val == 1) {
     decompress(kneeBitmap, VRAM_A,  LZ77Vram);
   }
+  printInterp();
 }
 
 void clearFullScreen(vector<touchPosition> &touchPositions) {
@@ -146,7 +199,6 @@ void clearScreenKeepingPoints(vector<touchPosition> const &touchPositions) {
     setPixel(it->px, it->py, 255);
   }
 }
-
 
 
 int main(void) {
@@ -165,8 +217,7 @@ int main(void) {
   
   touchPosition touchXY;
   touchPosition touchOld;
-  
-  
+
   float lambda = 1;
   
   videoSetMode(MODE_FB0);
@@ -175,27 +226,19 @@ int main(void) {
   lcdMainOnBottom();
   
   vector<touchPosition> touchArray;
-  
-  irqSet(IRQ_VBLANK, Vblank);
-
 	
 	touchRead(&touchOld);
 	
   //  section background
 	bgInit(3, BgType_Bmp16, BgSize_B16_256x256, 0,0);
-  // decompress(kneeBitmap, VRAM_A,  LZ77Vram);
 	
   consoleDemoInit();
-	
-  // iprintf("\x1b[10;0H(debug) Frame = %d",frame);
-  printf("\x1b[16;0HLambda = %0.2f",lambda);
+
 	
   while(1) {
 	
 		swiWaitForVBlank();
 		touchRead(&touchXY);
-		
-    // std::cout << touchXY.px;
 
     if(touchXY.px != 0 || touchXY.py != 0) {
       if(abs(touchXY.px - touchOld.px) > 3 && abs(touchXY.py - touchOld.py) > 3) {
@@ -206,74 +249,68 @@ int main(void) {
       }
     }
     
-    // iprintf("\x1b[10;0H(debug) Frame = %d",frame);
     printf("\x1b[16;0HLambda = %0.2f",lambda);
-    // iprintf("\x1b %d", int(touchArray.size()));
-    
-		// print at using ansi escape sequence \x1b[line;columnH 
-		// iprintf("\x1b[10;0HFrame = %d",frame);
-       // iprintf("\x1b[16;0HTouch x = %04X, %04X\n", touchXY.rawx, touchXY.px);
-      // iprintf("Touch y = %04X, %04X\n", touchXY.rawy, touchXY.py);    
-	
-	
-       // for(vector<touchPosition>::iterator it = touchArray.begin();it != touchArray.end(); ++it) {
-       //          iprintf("\x1b %d, %d", it->px, it->py);
-       //          
-       //        }
+    printInterp();
        
-       scanKeys();
-       if (keysHeld() & KEY_A) {
-         drawInterpolation(touchArray, lambda);
-         drawed = true;
-       }
-       if (keysHeld() & KEY_UP) {
-         lambda = lambda + 0.01;
-         if (lambda > 1)
-           lambda = 1;
-         printf("\x1b[16;0HLambda = %0.2f",lambda);
-       }
-       if (keysHeld() & KEY_DOWN) {
-         lambda = lambda - 0.01;
-         if (lambda < 0)
-           lambda = 0;
-         printf("\x1b[16;0HLambda = %0.2f",lambda);
-       }
-       if (keysHeld() & KEY_X) {
-         clearFullScreen(touchArray);
-         drawed = false;
-       }
-       if (keysHeld() & KEY_Y) {
+    scanKeys();
+    if (keysHeld() & KEY_A) {
+      drawInterpolation(touchArray, lambda);
+      drawed = true;
+    }
+    if (keysHeld() & KEY_UP) {
+      lambda = lambda + 0.01;
+      if (lambda > 1)
+        lambda = 1;
+      // printf("\x1b[16;0HLambda = %0.2f",lambda);
+    }
+    if (keysHeld() & KEY_DOWN) {
+      lambda = lambda - 0.01;
+      if (lambda < 0)
+        lambda = 0;
+      // printf("\x1b[16;0HLambda = %0.2f",lambda);
+    }
+    if (keysHeld() & KEY_X) {
+      clearFullScreen(touchArray);
+      drawed = false;
+    }
+    if (keysHeld() & KEY_Y) {
+     clearScreenKeepingPoints(touchArray);
+     drawed = false;
+    }
+    if (keysHeld() & KEY_LEFT) {
+      switch_val = switch_val - 1;
+      if (switch_val < 0) {
+        switch_val = 0;
+        } else {
+          clearScreenKeepingPoints(touchArray);
+          if (drawed)
+            drawInterpolation(touchArray, lambda);
+        }
+    }
+    if (keysHeld() & KEY_RIGHT) {
+      switch_val = switch_val + 1;
+      if (switch_val > 1) {
+        switch_val = 1;
+      } else {
         clearScreenKeepingPoints(touchArray);
-        drawed = false;
-       }
-       if (keysHeld() & KEY_LEFT) {
-         // touchArray.pop_back();
-         // clearScreenKeepingPoints(touchArray);
-         switch_val = switch_val - 1;
-         if (switch_val < 0) {
-           switch_val = 0;
-           } else {
-             clearScreenKeepingPoints(touchArray);
-             if (drawed)
-               drawInterpolation(touchArray, lambda);
-           }
-       }
-       if (keysHeld() & KEY_RIGHT) {
-         switch_val = switch_val + 1;
-         if (switch_val > 1) {
-           switch_val = 1;
-         } else {
-           clearScreenKeepingPoints(touchArray);
-           if (drawed)
-             drawInterpolation(touchArray, lambda);
-         }
-       }
-       
-       // if (touchArray.size() == 3 && !drawed) {
-       //   drawInterpolationLagrange(touchArray, 1);
-       //   drawed = true;
-       //  }
-	}
-  
+        if (drawed)
+          drawInterpolation(touchArray, lambda);
+      }
+    }
+    if (keysHeld() & KEY_L) {
+       interp_type = interp_type - 1;
+       if (interp_type < 1) {
+         interp_type = 0;
+         printInterp();
+       } 
+     }
+    if (keysHeld() & KEY_R) {
+      interp_type = interp_type + 1;
+      if (interp_type > 1) {
+        interp_type = 1;
+        printInterp();
+      } 
+    }
+  }  
   return 0;
 }
